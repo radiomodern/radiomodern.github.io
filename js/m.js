cc4cda797d5974de71d75de1b58879aec0c1120f
Yuriy Gatilin <modernptz@gmail.com>
2016-05-31 22:39:21 +0200

    Initial release

---
+++ js/m.js
@@ -0,0 +1,78 @@
+var min;
+var sec;
+var IntervalID;
+var timer;
+$(document).ready(function () {
+    initTimer();
+});
+
+$("a.soc").each(function () {
+    var _href = $(this).attr("href");
+    $(this).attr("href", _href + document.location.pathname);
+});
+
+function initTimer() {
+    if (!timer) {
+        timer = $('#time').html();
+    }
+
+    IntervalID = setInterval(
+        function () {
+            timer--;
+            sec = timer % 60;
+            min = (timer - sec) / 60;
+            if (sec < 10) sec = '0' + sec;
+            //$('#time').html( min + ':' + sec );
+
+            if (timer <= 0) {
+                clearInterval(IntervalID);
+                //$('#time').html('');
+                if (timer <= -100) {
+                    setTimeout(function () {
+                        getSong();
+                    }, 10000);
+                }
+                else {
+                    //$('#song').html('');
+                    getSong();
+                }
+            }
+        },
+        ( timer < 0 ? 2000 : 1000 )
+    );
+}
+
+function updateSongNew(song, time) {
+    $('#song').html(song);
+    if (!time || !song) {
+        setTimeout(function () {
+            getSong();
+        }, 5000);
+    }
+    else {
+        timer = time;
+        clearInterval(IntervalID);
+        initTimer();
+    }
+}
+
+function getSong() {
+    $.ajax(
+        {
+            url: '/ajax.php',
+            data: {getSong: "ok"},
+            dataType: 'json',
+            type: 'POST',
+            scriptCharset: 'utf-8',
+
+            success: function (json, textStatus) {
+                updateSongNew(json.song, json.time);
+
+            }
+        }
+    );
+}
+
+
+
+
---
+++ uploads/random/index.php
@@ -0,0 +1,13 @@
+<?php
+/*::::::::::::::::::::::::::::::::::::::::
+ System name: cpengine
+ Short Desc: Full Russian Security Power Pack
+ Version: 2.0 (Service Pack 2)
+ Authors:  Arcanum (php@211.ru) &  Censored!
+ Date: March 18, 2008
+::::::::::::::::::::::::::::::::::::::::*/
+
+header('Location:../index.php?id=2');
+exit;
+
+?>
